292048699cae170b112594da57b63f17f4c187e7
xiejy36 <1059256748@qq.com>
2020-09-17 16:20:53 +0800

    '初始化'

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Shopcart = () => import('../views/shopcart/Shopcart')
+const Profile = () => import('../views/profile/Profile')
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },{
+    path: '/shopcart',
+    component: Shopcart
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
